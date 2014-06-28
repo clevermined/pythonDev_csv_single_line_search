58e6c397c5e01c312018c104687b17822afc375e
hoonanwoo <hoonanwoo@wooHoo.org>
2014-06-28 09:16:10 -0400

    added python code

---
+++ csvSingle.py
@@ -0,0 +1,17 @@
+import csv
+
+def csvSingle(inputString,index):
+    """parse csv string. return string of specified index position.
+       check string length for out of list bounds error tolerance.
+       if index is at least on less than the length of input string.
+    """
+    try:
+        checkString = len(inputString) -1
+        if index < checkString:
+            csv_reader = csv.reader( [inputString] )
+            fields = None
+            for row in csv_reader:
+                fields = row
+            print( fields[index] )
+    except IndexError:
+        print("You have specified index: <", index, "> :which exceeds the range of the string: IndexError") 
